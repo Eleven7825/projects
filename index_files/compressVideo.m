ba5f517214f24af530c80fde7f55cc01fe911f05
Shiyi Chen <63940863+Eleven7825@users.noreply.github.com>
2021-07-03 20:24:44 +0800

    compress the videos

--- index_files/compressVideo.m
+++ index_files/compressVideo.m
@@ -1,11 +1,13 @@
-vidi = VideoReader("Dr_5e-02_q0_1e-08.mp4");
-aft = VideoWriter("Dr_5e-02_q0_1e-08_after.mp4",'MPEG-4');
+vidi = VideoReader("Dr_1e-01_q0_1e-07_scaled.mp4");
+aft = VideoWriter("Dr_1e-01_q0_1e-07_scaled_after.mp4",'MPEG-4');
 firstF = read(vidi,1);
 imshow(firstF);
 open(aft);
 [~, c] = imcrop(firstF);
-for i = 1: round(vidi.NumFrames/2)
-    frame = imcrop(read(vidi,i),c);
-    writeVideo(aft, frame);
+for i = 1: round(vidi.NumFrames)
+    if mod(i,3)==0
+        frame = imcrop(read(vidi,i),c);
+        writeVideo(aft, frame);
+    end
 end
 close(aft);
